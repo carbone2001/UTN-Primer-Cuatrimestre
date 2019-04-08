7239ad1e5171d393006ca805cb1adf738cc1c136
alumno <alumno>
2019-04-08 12:21:34 -0300

    clase 7 matrices bidimensionales

--- Programacion y laboratorio/Ejercicios/main.c
+++ Programacion y laboratorio/Ejercicios/main.c
@@ -1,13 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main()
 {
-   char cadena[50];
-   fflush(stdin);
-   fgets(cadena);
-   printf("%s",cadena);
+   char nombre[20]={"juan carlos raul"};
+   char apellido[20]={"perez"};
+   char nombreCompleto[41];
+   int i;
+   /*printf("Ingrese nombre: ");
+   gets(nombre);
+   printf("Ingrese apellido: ");
+   gets(apellido);*/
+   strcpy(nombreCompleto,apellido);
+   strcat(nombreCompleto,", ");
+   strcat(nombreCompleto,nombre);
+   strlwr(nombreCompleto);
+   nombreCompleto[0]=toupper(nombreCompleto[0]);
+   for(i=0;i<strlen(nombreCompleto);i++)
+   {
+       if(nombreCompleto[i]==' ')
+       {
+           nombreCompleto[i+1]=toupper(nombreCompleto[i+1]);
+       }
+   }
 
+
+   printf(nombreCompleto);
+
+   /*
+   strcat(nombreCompleto,nombre);
+   nombreCompleto[tamCadNo]=',';
+   nombreCompleto[tamCadNo+1]=' ';
+   nombreCompleto[tamCadNo+2]='\0';
+   strcat(nombreCompleto,apellido);
+   strlwr(nombreCompleto);
+   nombreCompleto[0]=toupper(nombreCompleto[0]);
+   nombreCompleto[tamCadNo+2]=toupper(nombreCompleto[tamCadNo+2]);
+   printf(nombreCompleto);
+   */
     return 0;
 }
